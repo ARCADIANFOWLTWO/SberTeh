64dacf0e684e84abc45e07e1842e00d96f209bf5
ARCADA <taoariya99@gmail.com>
2020-06-30 10:42:10 +0300

    Task дополнен

--- src/main/java/base/domain/Task.java
+++ src/main/java/base/domain/Task.java
@@ -3,6 +3,8 @@ package base.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Task {
@@ -14,9 +16,11 @@ public class Task {
     public Task() {
 
     }
+
     public Task(String name) {
         this(null, name);
     }
+
     public Task(Long uid, String name) {
         this.uid = uid;
         this.name = name;
@@ -36,5 +40,16 @@ public class Task {
 
     public String getName() {
         return name;
+    }
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "list")
+    private Set<Category> tasks;
+
+    public Set<Category> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Category> tasks) {
+        this.tasks = tasks;
     }
 }
