86aa903c7f23f62851bc388d5ac532742d2bba26
ARCADA <taoariya99@gmail.com>
2020-07-02 15:00:24 +0300

    Еще доделали

--- src/main/java/base/controllers/GreetingController.java
+++ src/main/java/base/controllers/GreetingController.java
@@ -2,16 +2,28 @@ package base.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+    /*@RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }*/
+    @RequestMapping(value="/greeting", method= RequestMethod.GET)
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
     }
-//<!--<a th:href=@{/index/{id}(id=${list.getId()})}" th:utext="${list.getName()}"></a>-->
+
+    @RequestMapping(value="/greeting", method=RequestMethod.POST)
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
+    }
+
 }
--- src/main/java/base/controllers/ListController.java
+++ src/main/java/base/controllers/ListController.java
@@ -1,19 +1,19 @@
 package base.controllers;
 
-import base.domain.List;
-import base.repository.ListRepository;
-import base.repository.TaskRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import base.domain.List;
+import base.repository.ListRepository;
+import base.repository.TaskRepository;
 
 @Controller
 public class ListController {
     @Autowired
     private ListRepository listRepository;
-
     @Autowired
     private TaskRepository taskRepository;
 
@@ -21,15 +21,31 @@ public class ListController {
     public String getIndex(Model model){
         Iterable<List> lists = listRepository.findAll();
 
-        model.addAttribute("lists", lists);
+        model.addAttribute("lists",lists);
+
         return "index";
     }
 
-    /*@RequestMapping(value = {"/index/{id}"}, method = RequestMethod.GET)
-    public String getIndex1(Model model){
-        Iterable<Task> tasks = taskRepository.findAll();
+    public static class Greeting {
 
-        model.addAttribute("tasks", tasks);
-        return "index";
-    }*/
+        private long id;
+        private String content;
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+    }
 }
