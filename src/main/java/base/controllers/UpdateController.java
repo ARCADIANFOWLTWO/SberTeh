3cc99f29514ddc9bd36197c3e8b94f584e47f7f4
ARCADA <taoariya99@gmail.com>
2020-07-16 10:57:12 +0300

    Есть какая-то ошибка в изменении тасков

--- src/main/java/base/controllers/UpdateController.java
+++ src/main/java/base/controllers/UpdateController.java
@@ -4,13 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import base.domain.ListE;
+import base.domain.TaskE;
 import base.repository.ListRepository;
-
+import base.repository.TaskRepository;
 
 @Controller
 public class UpdateController {
     @Autowired
     private ListRepository listRepository;
+    @Autowired
+    private TaskRepository taskRepository;
 
     @GetMapping(value={"/list/{id}/updateList"})
     public String updateListForm(Model model, @PathVariable long id) {
@@ -26,5 +29,22 @@ public class UpdateController {
         listRepository.save(listToUpdate);
         return "redirect:/list/" + id;
     }
+    @GetMapping(value = {"/task/{idt}/updateTask"})
+    public String updateTaskForm(Model model, @PathVariable long idt) {
+        TaskE task = taskRepository.findById(idt).get();
+        model.addAttribute("task", task);
 
+        return "/updateTask";
+    }
+
+    @RequestMapping(value = {"/task/{idt}/updateTask"}, method = {RequestMethod.POST})
+    public String updateTaskSubmit(Model model, @PathVariable long idt, @ModelAttribute("task") TaskE task) {
+        TaskE taskToUpdate = taskRepository.findById(idt).get();
+        taskToUpdate.setTitle(task.getTitle());
+        taskToUpdate.setDone(task.getDone());
+        taskRepository.save(taskToUpdate);
+
+        //return "redirect:/task/" + taskToUpdate.getParentId();
+        return "redirect:/list/" + taskToUpdate.getParentId();
+    }
 }
